776ebf39a828d4986fd01e703cebcc58c9c6a05a
rahat <70068895+youreachedrahat@users.noreply.github.com>
2025-06-01 11:28:18 +0530

    ..

--- app/conditions/[slug]/page.tsx
+++ app/conditions/[slug]/page.tsx
@@ -1,8 +1,7 @@
 // app/conditions/[slug]/page.tsx
 import { notFound } from "next/navigation";
 import Content from "./content"; // async component with slow data
-import * as allData from "@/components/data/index";
-import { MedicalCondition } from "@/types";
+import allData from "@/components/data/index";
 
 type Props = {
   params: Promise<{ slug: string }>;
@@ -20,19 +19,13 @@ export default async function ConditionsPage({ params }: Props) {
 
 async function getDataFromSlug(slug: string) {
   try {
-    // Normalize the slug by converting dashes to camelCase
     const dataKey = slug.replace(/-([a-z])/g, (_, letter) =>
       letter.toUpperCase()
     );
 
-    // Dynamically access the corresponding data and return or handle undefined
-    return (
-      (allData[
-        dataKey as keyof typeof allData
-      ] as unknown as MedicalCondition) || null
-    );
+    return allData[dataKey as keyof typeof allData] || null;
   } catch (error) {
-    console.error(`Error fetching data for slug "${slug}":`, error);
+    console.error(`Error fetching data for slug ${slug}:`, error);
 
     return null;
   }
