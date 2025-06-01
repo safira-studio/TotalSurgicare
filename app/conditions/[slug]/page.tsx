d9632627f6b57b59f9de64733a112d2b0bff37f9
rahat <70068895+youreachedrahat@users.noreply.github.com>
2025-06-01 11:25:26 +0530

    refactor: update import statement and enhance data fetching logic in ConditionsPage

--- app/conditions/[slug]/page.tsx
+++ app/conditions/[slug]/page.tsx
@@ -1,7 +1,8 @@
 // app/conditions/[slug]/page.tsx
 import { notFound } from "next/navigation";
 import Content from "./content"; // async component with slow data
-import allData from "@/components/data/index";
+import * as allData from "@/components/data/index";
+import { MedicalCondition } from "@/types";
 
 type Props = {
   params: Promise<{ slug: string }>;
@@ -19,13 +20,19 @@ export default async function ConditionsPage({ params }: Props) {
 
 async function getDataFromSlug(slug: string) {
   try {
+    // Normalize the slug by converting dashes to camelCase
     const dataKey = slug.replace(/-([a-z])/g, (_, letter) =>
       letter.toUpperCase()
     );
 
-    return allData[dataKey as keyof typeof allData] || null;
+    // Dynamically access the corresponding data and return or handle undefined
+    return (
+      (allData[
+        dataKey as keyof typeof allData
+      ] as unknown as MedicalCondition) || null
+    );
   } catch (error) {
-    console.error(`Error fetching data for slug ${slug}:`, error);
+    console.error(`Error fetching data for slug "${slug}":`, error);
 
     return null;
   }
